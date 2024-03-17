a687efc5753e34d70a1d66f1b86eb55e3e65eff7
Yan Cheng <ychenggithub@users.noreply.github.com>
2024-03-16 21:14:09 -0700

    update

---
+++ socket/simple_browser.py
@@ -0,0 +1,16 @@
+# ~/Documents/Study/Meta_Backend> python -m venv ./Django
+# ~/Documents/Study/Meta_Backend> source ./Django/bin/activate
+# deactivate 
+
+import socket
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org', 80))
+cmd = 'GET https://data.pr4e.org/page1.htm HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode(), end=' ')
